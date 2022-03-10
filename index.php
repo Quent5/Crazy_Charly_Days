af3b3e4b69b57938b83c7607ce415644a3aaaf04
Quent5 <dalbon3u@etu.univ-lorraine.fr>
2022-03-10 11:05:08 +0100

    Ajout page d'accueil et style

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 require 'vendor/autoload.php';
+require_once "src/vues/PageMaker.php";
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use custombox\vue\PageMaker;
 
 $configuration = [
     'settings' => [
@@ -20,13 +22,18 @@ $db->addConnection(parse_ini_file('src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-$app = new \Slim\App;
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+]);
 
-$app->get(
-    '/',
-    'mywishlist\controller\AffichageController:afficherAcceuil'
-)->setName('acceuil');
 
+$app->get('/',function (Request $request, Response $response, $args) {
+    $response->getBody()->write(PageMaker::startPage("Accueil"));
+    $response->getBody()->write(PageMaker::startHeader());
+
+});
 $app->get(
     '/test',
     function ($rq, $rs, $args) {
