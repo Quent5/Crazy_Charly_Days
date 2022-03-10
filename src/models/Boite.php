68cb926fcc9e988a5d11b2e41398f77f4e8d30f3
Gab <gabin.hemmerle6@etu.univ-lorraine.fr>
2022-03-10 10:19:41 +0100

    Init

---
+++ src/models/Boite.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace custombox\models;
+
+class Boite extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'boite';
+    protected $primaryKey = 'id' ;
+}
---
+++ src/models/Categorie.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace custombox\models;
+
+class Categorie extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'categorie';
+    protected $primaryKey = 'id' ;
+}
